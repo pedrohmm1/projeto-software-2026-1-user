25b7e1576f7b5265c57c703dbddac6d41fa4a144
pedrohmm1 <pedro.mourao.machado@gmail.com>
2026-03-09 11:00:36 -0300

    sync fork

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ from db import db
 from models import User
 import os
 
-
-postgres_user = os.environ['POSTGRES_USER']
-postgres_password = os.environ['POSTGRES_PASSWORD']
-postgres_url= os.environ['POSTGRES_URL']
-
-
+postgres_user = os.environ.get('POSTGRES_USER', 'appuser')
+postgres_password = os.environ.get('POSTGRES_PASSWORD', 'apppass')
+postgres_url = os.environ.get('POSTGRES_URL', 'localhost')
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{postgres_user}:{postgres_password}@{postgres_url}:5432/users"
@@ -34,6 +31,15 @@ def create_user():
         "email": user.email
     }), 201
 
+@app.route("/users/<uuid:user_id>", methods=["GET"])
+def get_user(user_id):
+    user = User.query.get_or_404(user_id)
+
+    return jsonify({
+        "id": str(user.id),
+        "name": user.name,
+        "email": user.email
+    }), 201
 
 @app.route("/users/<uuid:user_id>", methods=["DELETE"])
 def delete_user(user_id):
